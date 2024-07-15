54dcdf8b9b08a42250e304696f24a27ccbcae99e
zvecr <git@zvecr.com>
2024-07-15 16:54:44 +0100

    Adjust dark theme text color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -19,6 +19,9 @@ export default createVuetify({
           background: 'rgb(27, 27, 31)',
           surface: 'rgb(22, 22, 24)',
           primary: 'rgb(62, 99, 221)',
+
+          "on-background": 'rgba(255, 255, 245, 0.86)',
+          "on-surface": 'rgba(255, 255, 245, 0.86)',
         },
       },
     },
